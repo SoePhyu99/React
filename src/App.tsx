e02cd6f428ae1aac929ae4c1fc59dc47680ee311
Soe Phyu <soephyuphyuhtun99@gmail.com>
2023-06-20 15:50:56 +0630

    Styling Components - Vanilla CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 function App() {
-	let [state, setState] = useState(false);
+	let items = ["New York", "Paris", "London"];
+	let heading = "Miami";
+	let onSelectItem = (item: string) => {
+		console.log(item);
+	};
 	return (
-		<div>
-			{state && <Alert onClose={() => setState(false)}>My Alert</Alert>}
-			<Button color="dark" onClick={() => setState(true)}>
-				My Button
-			</Button>
-		</div>
+		<>
+			<ListGroup
+				items={items}
+				heading={heading}
+				onSelectItem={onSelectItem}
+			></ListGroup>
+		</>
 	);
 }
 
